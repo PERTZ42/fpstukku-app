180f66dc9b9c1d802e588905549c00796fa93841
Perttu Nurmi <perttu.nurmi+github@gmail.com>
2024-06-17 08:19:50 +0300

    alku

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <div><a href="Home#"> Etusivu </a></div>
+          <div><a href=""> Info </a></div>
+        </nav>
+        <div className='jakaja'>
+          <div className='tekstialue'>
+            <h3>Tekstialue</h3>
+          </div>
+          <div className='treenikalenteri'>
+            <h3>Treenikalenteri</h3>
+            <iframe className='kalenteri' src="https://calendar.google.com/calendar/embed?src=fpstukku%40gmail.com&ctz=Europe%2FHelsinki" width={600} height={600}></iframe>
+          </div>
+        </div>
       </header>
     </div>
   );
