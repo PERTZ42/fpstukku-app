3c1695c146cc8385a36452cdc42d2a9afc102510
Perttu Nurmi <perttu.nurmi+github@gmail.com>
2024-06-18 21:12:41 +0300

    Etusivun rakenne
    Sivun rakenne muodostuu pikkuhiljaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
 import './App.css';
 
+function Paavalikko() {
+  return (
+    <nav>
+      <a href="."> Etusivu </a>
+      <a href="."> Kalenteri </a>
+      <a href="."> Palvelimet </a>
+      <a href="."> Sovellukset </a>
+      <a href='.'> Info </a>
+    </nav>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <h2> FPSTUKKU </h2>
+    </footer>
+  )
+}
+
+function Kalenteri() {
+  return (
+    <div className='treenikalenteri'>
+      <h3>Treenikalenteri</h3>
+      <iframe title='kalenteri' src="https://calendar.google.com/calendar/embed?src=fpstukku%40gmail.com&ctz=Europe%2FHelsinki" width={600} height={600}> </iframe>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav>
-          <div><a href="Home#"> Etusivu </a></div>
-          <div><a href=""> Info </a></div>
-        </nav>
-        <div className='jakaja'>
-          <div className='tekstialue'>
-            <h3>Tekstialue</h3>
-          </div>
-          <div className='treenikalenteri'>
-            <h3>Treenikalenteri</h3>
-            <iframe className='kalenteri' src="https://calendar.google.com/calendar/embed?src=fpstukku%40gmail.com&ctz=Europe%2FHelsinki" width={600} height={600}></iframe>
-          </div>
-        </div>
+
+        <Paavalikko></Paavalikko>
+        <Kalenteri></Kalenteri>
       </header>
-    </div>
-  );
+
+
+      <Footer></Footer>
+    </div >
+  )
 }
 
 export default App;
