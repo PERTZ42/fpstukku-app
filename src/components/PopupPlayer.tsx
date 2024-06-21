9d7c5a9b7330a1bf0a01a6986a1dba2ad4e3de1a
Perttu Nurmi <perttu.nurmi+github@gmail.com>
2024-06-22 00:37:08 +0300

    paranee

--- src/components/PopupPlayer.tsx
+++ src/components/PopupPlayer.tsx
@@ -4,8 +4,8 @@ import './popupplayer.css'
 function PopupPlayer(props: any) {
 	return (props.trigger) ? (
 		<div className='video-popup'>
-			<div className='button'>
-				<button onClick={() => props.setTrigger(false)}> X </button>
+			<div className='button' onClick={() => props.setTrigger(false)}>
+				X
 			</div>
 			<div className='popup-inner'>
 				<iframe width="1200" height="800" src="https://www.youtube.com/embed/jwsoT2diqSc?si=vFn-PtFKdKy3OqIN&autoplay=1;controls=0" title="YouTube video player"></iframe>
